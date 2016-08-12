fd92c71f1f351e668b6161736fed28aac8e28066
Balta-zar <bojan.baltic@gmail.com>
2016-08-12 13:37:39 +0200

    Update run.py

--- run.py
+++ run.py
@@ -1,41 +1,53 @@
-import tornado.httpserver
+import tornado.web
 import tornado.websocket
 import tornado.ioloop
-import tornado.web
-import socket
-import datetime
+from tornado import gen
 
- 
-class WSHandler(tornado.websocket.WebSocketHandler):
+import time
+
+class WebSocketHandler(tornado.websocket.WebSocketHandler):
     def open(self):
-        print 'new connection'
-        self.write_message('<b>Hi client, I will start to send values from sensor on every 2 sec</b>')
-        tornado.ioloop.IOLoop.instance().add_timeout(datetime.timedelta(seconds=2), self.test)
-      
-    def on_message(self, message):
-        if (message == ''):
-            message = 'blank message'
-        print 'message received: ', message
-        self.write_message('Hey client, you sent me <b>'+message+'</b')
- 
+        self._break = False
+        print "New client connected"
+        self.write_message("You are connected")
+        # tornado.ioloop.IOLoop.current().spawn_callback(self.send_values_from_sensor)
+        self.send_values_from_sensor()
+        
+        
+    # This is Real Time function. Explanation on bottom of page:
+    # http://www.tornadoweb.org/en/stable/guide/coroutines.html
+    @gen.coroutine
+    def send_values_from_sensor(self):
+        while True:
+            if self._break:
+                break
+            nxt = gen.sleep(1) # Start the clock.
+            yield self.send_message() # Run while the clock is ticking.
+            yield nxt # Wait for the timer to run out.
+    
+    def send_message(self):
+        time.sleep(0.9) # Simulate calculation
+        return self.write_message('hi')
+            
+    def on_messagee(self, message):
+        self.write_message(message)
+        
     def on_close(self):
-        print 'connection closed'
- 
+        self._break = True
+        print "Client disconnected"
+        
     def check_origin(self, origin):
+        # allow_origin = self.server.settings.get("websocket_allow_origin", "*")
+        # if allow_origin == "*":
+        #     return True
         return True
     
-    def test(self):
-        self.write_message("New value from sensor")
-        tornado.ioloop.IOLoop.instance().add_timeout(datetime.timedelta(seconds=2), self.test)
- 
+    
 application = tornado.web.Application([
-    (r'/getValue', WSHandler),
+    (r"/getValue", WebSocketHandler),
 ])
- 
- 
-if __name__ == "__main__":
-    http_server = tornado.httpserver.HTTPServer(application)
-    http_server.listen(8000)
-    myIP = socket.gethostbyname(socket.gethostname())
-    print 'Websocket Server Started at localhost:8000' 
+
+if __name__ == '__main__':
+    application.listen(8000)
+    print "Web socket server listen on localhost:8000"
     tornado.ioloop.IOLoop.instance().start()
